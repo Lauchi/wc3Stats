0624e076ed280794f366145ee0d058498a9e027d
Lauchi <simonheiss87@gmail.com>
2018-09-15 22:04:57 +0200

    ec

--- Adapters.w3gFiles/W3GFileReader.cs
+++ Adapters.w3gFiles/W3GFileReader.cs
@@ -14,10 +14,10 @@ namespace Adapters.w3gFiles
         {
             _mapping = mapping;
         }
+
         public Wc3Game Read(string filePath)
         {
-
-            byte[] fileBytes = File.ReadAllBytes(filePath);
+            var fileBytes = File.ReadAllBytes(filePath);
             _mapping.SetBytes(fileBytes);
 
             var expansionType = _mapping.GetExpansionType();
@@ -36,12 +36,13 @@ namespace Adapters.w3gFiles
 
         public ExpansionType GetExpansionType()
         {
-            var expansionType = Encoding.UTF8.GetString(new [] { _fileBytes[51], _fileBytes[50] , _fileBytes[49], _fileBytes[48] });
+            var expansionType = Encoding.UTF8.GetString(new[]
+                {_fileBytes[51], _fileBytes[50], _fileBytes[49], _fileBytes[48]});
             switch (expansionType)
             {
-                    case "W3XP": return ExpansionType.TheFrozenThrone;
-                    case "WAR3": return ExpansionType.ReignOfChaos;
-                   default: throw new ArgumentException($"No correct Game Type found: {expansionType}");
+                case "W3XP": return ExpansionType.TheFrozenThrone;
+                case "WAR3": return ExpansionType.ReignOfChaos;
+                default: throw new ArgumentException($"No correct Game Type found: {expansionType}");
             }
         }
 
@@ -58,9 +59,9 @@ namespace Adapters.w3gFiles
             var uInt32 = _fileBytesHeader.Word(0x000A);
             switch (uInt32)
             {
-                    case 0x0 : return PlayerMode.SinglePlayer;
-                    case 0x8000 : return PlayerMode.MultiPlayer;
-                    default: throw new ArgumentException($"No correct MultiPlayerMode found: {uInt32}");
+                case 0x0: return PlayerMode.SinglePlayer;
+                case 0x8000: return PlayerMode.MultiPlayer;
+                default: throw new ArgumentException($"No correct MultiPlayerMode found: {uInt32}");
             }
         }
 
@@ -73,41 +74,38 @@ namespace Adapters.w3gFiles
         public TimeSpan GetPlayedTime()
         {
             var milliseconds = _fileBytesHeader.DWord(0x000C);
-            var timeSpan = new TimeSpan(0,0,0,0,(int) milliseconds);
+            var timeSpan = new TimeSpan(0, 0, 0, 0, (int) milliseconds);
             return timeSpan;
         }
     }
 
     public enum PlayerMode
     {
-        MultiPlayer, SinglePlayer
+        MultiPlayer,
+        SinglePlayer
     }
 
     public class GameVersion
     {
-        public uint MajorVersion { get; }
-        public ushort BuildVersion { get; }
-        public string AsString => $"1.{MajorVersion}.{BuildVersion}";
-
         public GameVersion(uint majorVersion, ushort buildVersion)
         {
             MajorVersion = majorVersion;
             BuildVersion = buildVersion;
         }
+
+        public uint MajorVersion { get; }
+        public ushort BuildVersion { get; }
+        public string AsString => $"1.{MajorVersion}.{BuildVersion}";
     }
 
     public enum ExpansionType
     {
-        TheFrozenThrone, ReignOfChaos
+        TheFrozenThrone,
+        ReignOfChaos
     }
 
     public class Wc3Game
     {
-        public ExpansionType ExpansionType { get; }
-        public GameVersion Version { get; }
-        public PlayerMode PlayerMode { get; }
-        public TimeSpan GameTime { get; }
-
         public Wc3Game(ExpansionType expansionType, GameVersion version, PlayerMode playerMode, TimeSpan gameTime)
         {
             ExpansionType = expansionType;
@@ -115,6 +113,11 @@ namespace Adapters.w3gFiles
             PlayerMode = playerMode;
             GameTime = gameTime;
         }
+
+        public ExpansionType ExpansionType { get; }
+        public GameVersion Version { get; }
+        public PlayerMode PlayerMode { get; }
+        public TimeSpan GameTime { get; }
 
         public IEnumerable<Player> Players { get; }
     }
